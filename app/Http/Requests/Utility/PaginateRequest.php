7103d8643a6b4a45a7b376fc0aff5673c3e93ce3
daniil <daniil@apptica.com>
2026-06-12 10:39:57 +0300

    T-53-RecentlyAdded: add PaginateRequest

---
+++ app/Http/Requests/Utility/PaginateRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests\Utility;
+
+use App\Enums\SearchTypes;
+use Illuminate\Auth\Events\Lockout;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
+
+class PaginateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'page' => 'sometimes|integer|min:1',
+            'perPage' => 'sometimes|integer|max:50|min:10',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'messages' => [
+                'name.required' => 'Название трека обязательно для заполнения.',
+                'name.string' => 'Название трека должно быть строкой.',
+
+                'artists.required' => 'Укажите хотя бы одного артиста.',
+                'artists.array' => 'Список артистов должен быть массивом.',
+                'artists.*.id.integer' => 'Идентификатор артиста должен быть числом.',
+                'artists.*.name.required' => 'Имя каждого артиста обязательно.',
+                'artists.*.name.string' => 'Имя артиста должно быть строкой.',
+            ],
+        ];
+    }
+
+}
